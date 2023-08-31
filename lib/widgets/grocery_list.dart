cf4af6571c1dc62754ed6f7ba151fc920b2e00fe
dungphamWeallnet <dung.pham@weallnet.com>
2023-08-31 18:23:59 +0700

    creating and connecting a dummy backend (using Firebase)

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/widgets/new_item.dart';
 
@@ -10,12 +14,63 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https('shopping-list-app-d8930-default-rtdb.firebaseio.com',
+        'shopping_list.json');
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Something went wrong! Please try again later.';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (category) => category.value.title == item.value['category'])
+            .value;
+        loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ));
+      }
+      setState(() {
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (err) {
+      setState(() {
+        _error = 'Something went wrong! Please try again later.';
+      });
+    }
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
         MaterialPageRoute(builder: (context) => const NewItem()));
-
+    // _loadItems();
     if (newItem == null) {
       return;
     }
@@ -24,33 +79,62 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem grocery) {
+  void _removeItem(GroceryItem grocery) async {
     final groceryIndex = _groceryItems.indexOf(grocery);
+
     setState(() {
       _groceryItems.remove(grocery);
     });
-    ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-          duration: const Duration(seconds: 5),
-          content: const Text('Expense deleted.'),
-          action: SnackBarAction(
-            label: 'Undo',
-            onPressed: () {
-              setState(() {
-                _groceryItems.insert(groceryIndex, grocery);
-              });
-            },
-          )),
-    );
+
+    final url = Uri.https('shopping-list-app-d8930-default-rtdb.firebaseio.com',
+        'shopping_list/${grocery.id}.json');
+
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      // ignore: use_build_context_synchronously
+      if (!context.mounted) {
+        return;
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            duration: Duration(seconds: 5),
+            content: Text('Something went wrong!')),
+      );
+      setState(() {
+        _groceryItems.insert(groceryIndex, grocery);
+      });
+    }
+
+    // ScaffoldMessenger.of(context).clearSnackBars();
+    // ScaffoldMessenger.of(context).showSnackBar(
+    //   SnackBar(
+    //       duration: const Duration(seconds: 5),
+    //       content: const Text('Expense deleted.'),
+    //       action: SnackBarAction(
+    //         label: 'Undo',
+    //         onPressed: () {
+    //           setState(() {
+    //             _groceryItems.insert(groceryIndex, grocery);
+    //           });
+    //         },
+    //       )),
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(child: Text('No items added yet.'));
 
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
+          //reverse: true,
           itemCount: _groceryItems.length,
           itemBuilder: (context, index) => Dismissible(
                 key: ValueKey(_groceryItems[index].id),
@@ -69,6 +153,10 @@ class _GroceryListState extends State<GroceryList> {
                   trailing: Text(_groceryItems[index].quantity.toString()),
                 ),
               ));
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
@@ -19,14 +23,35 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  var _isSending = false;
+
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      print(_enteredName);
-      print(_enteredQuantity);
-      print(_selectedCategory);
+      setState(() {
+        _isSending = true;
+      });
+      final url = Uri.https(
+          'shopping-list-app-d8930-default-rtdb.firebaseio.com',
+          'shopping_list.json');
+      final response = await http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title
+          }));
+
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      print(response.statusCode);
+      print(response.body);
+
+      if (!context.mounted) {
+        return;
+      }
       Navigator.of(context).pop(GroceryItem(
-          id: DateTime.now().toString(),
+          id: resData['name'],
           name: _enteredName,
           quantity: _enteredQuantity,
           category: _selectedCategory));
@@ -124,9 +149,17 @@ class _NewItemState extends State<NewItem> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     TextButton(
-                        onPressed: _resetForm, child: const Text('Reset')),
+                        onPressed: _isSending ? null : _resetForm,
+                        child: const Text('Reset')),
                     ElevatedButton(
-                        onPressed: _saveItem, child: const Text('Add Item'))
+                        onPressed: _isSending ? null : _saveItem,
+                        child: _isSending
+                            ? const SizedBox(
+                                width: 16,
+                                height: 16,
+                                child: CircularProgressIndicator(),
+                              )
+                            : const Text('Add Item'))
                   ],
                 )
               ],
